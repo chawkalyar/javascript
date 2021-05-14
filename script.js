4e462797d9ee014d8ab74aa7af394dbfb95c154e
chawkalyar <64894469+chawkalyar@users.noreply.github.com>
2021-05-14 18:01:29 +0630

    Delete Folder

---
+++ script.js
@@ -0,0 +1,37 @@
+// // // // var a=4;
+// // // // var b=6;
+// // // //  var ans=10;
+// // // // console.log(`First number is ${a} Second number is ${b} Answer is ${ans}`)
+
+// // // var num1=2;
+// // // var num2=5;
+// // // var ans=num1-num2;
+// // // console.log(ans);
+
+// // var person={firstname:"John",lastname:"Doe"};
+// // console.log(person.firstname+" "+person.lastname);
+
+
+// function add(a,b){
+//     if(a==b){
+//         a**3;
+//     }
+//     else
+//      { a+b;}
+    
+//
+    function add(a,b){
+    if (a==b) {
+    return a*3;
+    }
+    else{
+    return a+b;
+    }
+    
+  }
+  console.log(add(2,2)); 
+  
+  
+
+  
+
